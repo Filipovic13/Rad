3f01f33a6ab41a5931f71f9c1a0a3ccbe801693b
fsaki13@gmail.com <fsaki13@gmail.com>
2022-09-14 17:26:06 +0200

    Decision tree - binary #2

--- Decision_tree_binary.R
+++ Decision_tree_binary.R
@@ -50,8 +50,9 @@ compute_eval_measures <- function(cm){
 
 eval1 <- compute_eval_measures(cm1)
 
-################################   2nd default balanced  ####################################
-
+#################################################################################
+#install.packages("ROSE")
+library(ROSE)
 
 tr_ctrl <- trainControl(method = "repeatedcv", repeats = 5, 
                         classProbs = TRUE,
@@ -83,6 +84,18 @@ up <- train(x = train_data[,-19],
 up$bestTune$cp
 
 
+tr_ctrl$sampling <- "rose"
+
+set.seed(33)
+rose <- train(x = train_data[,-19],
+            y = train_data$Diabetes,
+            method = "rpart",
+            metric = "ROC",
+            trControl = tr_ctrl,
+            tuneGrid = cp_grid)
+
+
+
 
 tr_ctrl$sampling <- NULL
 
@@ -99,7 +112,8 @@ best_cp <- 0.001
 
 models <- list(original = original,
                down = down,
-               up = up)
+               up = up,
+               rose = rose)
 
 inside_resampling <- resamples(models)
 summary(inside_resampling, metric = "ROC")
@@ -107,7 +121,7 @@ summary(inside_resampling, metric = "ROC")
 # Call:
 #   summary.resamples(object = inside_resampling, metric = "ROC")
 # 
-# Models: original, down, up 
+# Models: original, down, up, rose 
 # Number of resamples: 50 
 # 
 # ROC 
@@ -115,8 +129,11 @@ summary(inside_resampling, metric = "ROC")
 # original 0.6969897 0.7148407 0.7196613 0.7211747 0.7296718 0.7374346    0
 # down     0.7549067 0.7693298 0.7785918 0.7757327 0.7817193 0.7909056    0
 # up       0.7549618 0.7646936 0.7675204 0.7697824 0.7767960 0.7874405    0
+# rose     0.7549285 0.7663463 0.7726934 0.7736282 0.7826455 0.7903989    0
+##################################################################################
 
 
+### downsample
 set.seed(33)
 down_balanced_train_data <- downSample(x = train_data[, -19],
                                        y = train_data$Diabetes)
@@ -127,6 +144,19 @@ down_balanced_train_data = down_balanced_train_data[sample(1:nrow(down_balanced_
 table(down_balanced_train_data$Diabetes)
 
 
+### rose
+
+rose_balanced_train_data <- ROSE(Diabetes~., data=train_data, seed=33)$data
+
+table(rose_balanced_train_data$Diabetes)
+ggplot(data = rose_balanced_train_data,
+       mapping = aes(x= Diabetes)) +
+  geom_bar(position = "dodge")+
+  theme_light()
+
+
+
+################################   2nd default balanced  ####################################
 
 ###Tree2: downSample - default
 set.seed(33)
@@ -145,32 +175,14 @@ cm2
 
 eval2 <- compute_eval_measures(cm2)
 
-
-
-######################## 3rd - Tuned Balanced #######################
-
-tr_ctrl <- trainControl(method = 'repeatedcv',
-                        number = 10,
-                        repeats = 5)
-
-cp_grid <- expand.grid(.cp = seq(0.001, 0.1, 0.0005))
-
-set.seed(33)
-tree3_cv <- train(x = down_balanced_train_data[,-19],
-                  y = down_balanced_train_data$Diabetes,
-                  method = 'rpart',
-                  trControl = tr_ctrl,
-                  tuneGrid = cp_grid)
-tree3_cv$bestTune$cp
-
-
-
-####Tree3: downsample - tuned
+################################################################################
+#Tree3: rose - default
 set.seed(33)
 tree3 <- rpart(formula = Diabetes ~ ., 
-               data = down_balanced_train_data,
-               control = rpart.control(cp =best_cp))
+               data = rose_balanced_train_data)
 rpart.plot(tree3, extra = 104)
+
+tree3$control$cp
 
 tree3_pred <- predict(tree3, newdata = test_data, type = 'class')
 
@@ -182,6 +194,78 @@ cm3
 eval3 <- compute_eval_measures(cm3)
 
 
-data.frame(rbind(eval1,eval2,eval3),row.names = c("Imbalanced","Balanced default","Balanced tuned"))
+
+######################## 3rd - Tuned Balanced #######################
+
+
+tr_ctrl <- trainControl(method = 'repeatedcv',
+                        number = 10,
+                        repeats = 5)
+
+cp_grid <- expand.grid(.cp = seq(0.001, 0.1, 0.0005))
+
+
+
+####Tree4: downsample - tuned
+set.seed(33)
+tree4_cv <- train(x = down_balanced_train_data[,-19],
+                  y = down_balanced_train_data$Diabetes,
+                  method = 'rpart',
+                  trControl = tr_ctrl,
+                  tuneGrid = cp_grid)
+tree4_cv$bestTune$cp
+
+
+set.seed(33)
+tree4 <- rpart(formula = Diabetes ~ ., 
+               data = down_balanced_train_data,
+               control = rpart.control(cp =best_cp))
+rpart.plot(tree4, extra = 104)
+
+tree4_pred <- predict(tree4, newdata = test_data, type = 'class')
+
+cm4 <- table(actual=test_data$Diabetes,
+             prediceted= tree4_pred)
+cm4
+
+
+eval4 <- compute_eval_measures(cm4)
+################################################################
+
+
+
+####Tree5: rose - tuned
+set.seed(33)
+tree5_cv <- train(x = rose_balanced_train_data[,-19],
+                  y = rose_balanced_train_data$Diabetes,
+                  method = 'rpart',
+                  trControl = tr_ctrl,
+                  tuneGrid = cp_grid)
+tree5_cv$bestTune$cp
+
+
+
+####Tree5: rose - tuned
+set.seed(33)
+tree5 <- rpart(formula = Diabetes ~ ., 
+               data = down_balanced_train_data,
+               control = rpart.control(cp =best_cp))
+rpart.plot(tree5, extra = 104)
+
+tree5_pred <- predict(tree5, newdata = test_data, type = 'class')
+
+cm5 <- table(actual=test_data$Diabetes,
+             prediceted= tree5_pred)
+cm5
+
+
+eval5 <- compute_eval_measures(cm5)
+
+
+
+
+
+################################################
+data.frame(rbind(eval1,eval2,eval4,eval3,eval5),row.names = c("imbalanced","down default","down tuned","rose default","rose tuned"))
 
 
